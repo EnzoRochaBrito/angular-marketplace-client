b27711861d404e7ea9dc2ed48fed9ff68776de9c
enzo <enzo1brich@gmail.com>
2025-10-18 01:02:03 -0300

    changing user login status locally

--- src/app/interceptor/jwt/jwt-interceptor.ts
+++ src/app/interceptor/jwt/jwt-interceptor.ts
@@ -5,10 +5,12 @@ import { applicationTokens } from '../../utils/tokens';
 import { catchError, Observable, Subscription, switchMap, throwError } from 'rxjs';
 import { AuthService } from '../../service/auth/auth';
 import { Router } from '@angular/router';
+import { UserService } from '../../service/user/user';
 
 export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
   const cookieService = inject(CookieService)
   const authService = inject(AuthService)
+  const userService = inject(UserService)
   const accessToken = cookieService.getCookie(applicationTokens.jwt.ACCESS_TOKEN)
   const authReq = req.clone({
       setHeaders: {
@@ -22,7 +24,8 @@ export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
           switchMap(v => {
             const newToken = v.body?.acessToken
             if (!newToken) return throwError(() => err);
-            cookieService.setCookie(applicationTokens.jwt.ACCESS_TOKEN, newToken, 5*60)
+            cookieService.setCookie(applicationTokens.jwt.ACCESS_TOKEN, newToken, applicationTokens.jwt.ACCESS_TOKEN_EXPIRATION)
+            userService.isLogged.set(true)
 
             const newAuthReq = req.clone({
               setHeaders: {
@@ -34,6 +37,7 @@ export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
           catchError((refreshErr: HttpErrorResponse) => {
             if (refreshErr.status === 403) {
               const router = inject(Router)
+              userService.isLogged.set(false)
               router.navigate(['/login'])
               // logout locally
             }
