f58f101faf1ae1573eed23967259b383a0dc73fe
enzo <enzo1brich@gmail.com>
2025-10-18 01:06:21 -0300

    jwt:access_token_expiration

--- src/app/utils/tokens.ts
+++ src/app/utils/tokens.ts
@@ -1,6 +1,10 @@
 export const applicationTokens = {
     jwt: {
-        ACCESS_TOKEN: 'accessToken'
-        // refreshToken is httpOnly
+        ACCESS_TOKEN: 'accessToken',
+        /**
+         * 5 minutes
+         */
+        ACCESS_TOKEN_EXPIRATION: 5 * 60
+        // refreshToken is httpOnly,
     }
 }
