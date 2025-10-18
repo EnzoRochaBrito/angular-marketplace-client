09ffc70a062ee1bc6b8155e7163a848783d3f383
enzo <enzo1brich@gmail.com>
2025-10-18 01:08:17 -0300

    refresh rotation to verify if the user is logged during first access/page refresh/new tab

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,9 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserService } from './service/user/user';
+import { AuthService } from './service/auth/auth';
+import { CookieService } from './service/cookie/cookie';
+import { applicationTokens } from './utils/tokens';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +11,28 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
+  
   protected readonly title = signal('VerdiQ');
+  authService   = inject(AuthService);
+  cookieService = inject(CookieService)
+  userService   = inject(UserService)
+
+  ngOnInit(): void {
+    this.refreshRotation()
+  }
+
+  private refreshRotation() {
+    const refreshRotationSubscription = this.authService.refreshRotation().subscribe({
+      next: (r) => {
+        if (!r.body) return;
+        const acessToken = r.body.acessToken
+        this.cookieService.setCookie(applicationTokens.jwt.ACCESS_TOKEN, acessToken, applicationTokens.jwt.ACCESS_TOKEN_EXPIRATION);
+        this.userService.isLogged.set(true)
+      },
+      complete: () => {
+        refreshRotationSubscription.unsubscribe()
+      }
+    })
+  }
 }
