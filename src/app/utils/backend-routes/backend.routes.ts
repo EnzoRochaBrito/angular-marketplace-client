4ad1c327c429b509da0edfc34e355169c22de421
enzo <enzo1brich@gmail.com>
2025-10-17 23:12:47 -0300

    updating backend.routes object

--- src/app/utils/backend-routes/backend.routes.ts
+++ src/app/utils/backend-routes/backend.routes.ts
@@ -2,17 +2,25 @@
 const apiUrl = 'http://localhost:3001'
 
 export const server = {
-   api: {
-    base: apiUrl,
-    auth: {
-        base:            apiUrl + `/auth`,
-        login:           apiUrl + `/auth/login`,
-        register:        apiUrl + `/auth/signin`,
-        logout:          apiUrl + `/auth/logout`,
-        refreshRotation: apiUrl + `/auth/refresh-rotation`,
-    },
-    user: {
-        base: apiUrl + `/user`,
+    api: {
+        base: apiUrl,
+        auth: {
+            base:            apiUrl + `/auth`,
+            login:           apiUrl + `/auth/login`,
+            register:        apiUrl + `/auth/signin`,
+            logout:          apiUrl + `/auth/logout`,
+            refreshRotation: apiUrl + `/auth/refresh-rotation`,
+        },
+        user: {
+            base: apiUrl + `/user`,
+        },
+        product: {
+            base: apiUrl + `/product`,
+            id: (id: string) => apiUrl + `/product/${id}`
+        },
+        store: {
+            base: apiUrl + `/store`,
+            storeId: (id: string) => apiUrl + `/store/${id}`
+        }
     }
-   }
 }
