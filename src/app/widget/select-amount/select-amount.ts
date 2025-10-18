cd747f51fd86adc6a2d82806ee46837c74deb7b2
enzo <enzo1brich@gmail.com>
2025-10-17 23:22:48 -0300

    optional max value threshold

--- src/app/widget/select-amount/select-amount.ts
+++ src/app/widget/select-amount/select-amount.ts
@@ -12,6 +12,7 @@ export class SelectAmount implements OnInit, OnDestroy {
 
 @Input() negativeValue: boolean = false;
 @Input() initialValue!: number;
+@Input() maxValue!: number;
 @Output() outputAmount: EventEmitter<number> = new EventEmitter<number>();
 
 amount = new FormControl(1, [Validators.min(1)])
@@ -30,6 +31,7 @@ ngOnInit(): void {
   this.selectMinAmountSubscription = this.selectAmountFocused$.subscribe(v => {
     if (v) return;
     const selectProductAmount = this.amount.value
+    if (selectProductAmount! > this.maxValue) this.amount.setValue(this.maxValue);
     if (!selectProductAmount || (selectProductAmount < 1 && !this.negativeValue)) {
       this.amount.setValue(1)
     }
@@ -50,6 +52,7 @@ ngOnDestroy(): void {
 
 increase() {
   const value = this.amount.value! + 1
+  if (this.maxValue && value === this.maxValue + 1) return;
   this.amount.setValue(value)
 }
 
