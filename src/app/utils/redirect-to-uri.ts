6e249d7c822d8a2229e9dd7c3f46f9e1ad75dec4
enzo <enzo1brich@gmail.com>
2025-10-17 23:18:40 -0300

    service/directive to prevent Angular object reutilization

---
+++ src/app/utils/redirect-to-uri.ts
@@ -0,0 +1,34 @@
+import { Directive, ElementRef, HostListener, Injectable, Input } from "@angular/core";
+import { Router } from "@angular/router";
+
+@Injectable({
+    providedIn: 'root'
+})
+/**
+ * Service to redirect to an uri. Prevents Angular's component reutilization
+ */
+export class RedirectToUri {
+    constructor(private router: Router) { }
+    
+    redirectTo(uri: string) {
+        this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+          this.router.navigate([uri])});
+      }
+}
+
+@Directive({
+    selector: '[redirectToUri]'
+})
+/**
+ * Directive to redirect to an uri. Prevents Angular's component reutilization
+ */
+export class RedirectToUriDirective {
+    @Input() redirectToUri!: string
+    constructor(private el: ElementRef, private router: Router) { }
+
+    @HostListener('click')
+    redirectTo() {
+        this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+          this.router.navigate([this.redirectToUri])});
+    }
+}
