54700202a816e4e086666aaf0950abe52bfe45b1
enzo <enzo1brich@gmail.com>
2025-10-20 21:31:12 -0300

    cart item widget updating product amount and deleting

--- src/app/widget/cart-item/cart-item.ts
+++ src/app/widget/cart-item/cart-item.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { SelectAmount } from '../select-amount/select-amount';
 import { MaxStrLenPipe } from '../../pipe/max-str-len-pipe';
+import { CartService } from '../../service/cart/cart';
 
 @Component({
   selector: 'cart-item',
@@ -11,11 +12,17 @@ import { MaxStrLenPipe } from '../../pipe/max-str-len-pipe';
 })
 export class CartItem implements OnInit {
 @Input() itemId!: string;
+@Input() cartItemId!: string;
 @Input() itemImage!: string;
 @Input() itemName!: string;
 @Input() itemAmount!: number;
 @Input() itemPrice!: number;
+/**
+ * ### Emits the cart item id
+ */
 @Output() deleteEvent: EventEmitter<string> = new EventEmitter<string>()
+
+constructor(private cartService: CartService) { }
 
 totalPrice!: number;
 
@@ -23,12 +30,22 @@ ngOnInit(): void {
   this.totalPrice = this.itemAmount * this.itemPrice
 }
 
-amount(n: number) {
-  this.itemAmount = n
+updateAmount(amount: number) {
+  this.itemAmount = amount
   this.totalPrice = this.itemAmount * this.itemPrice
+  this.cartService.updateCartItemAmount(this.cartItemId!, amount)
 }
 
 delete() {
-  this.deleteEvent.emit(this.itemId)
+  const subscription = this.cartService.deleteCartItem(this.cartItemId).subscribe({
+    next: (v) => {
+      if (!v.body) return;
+      console.log(v.body)
+      this.deleteEvent.emit(this.cartItemId) // emits event to CartItemsPage
+    },
+    complete: () => {
+      subscription.unsubscribe()
+    }
+  })
 }
 }
