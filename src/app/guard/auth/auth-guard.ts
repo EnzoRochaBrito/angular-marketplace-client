2bc4c9c97b31c617368709ab10027a890b3ad4fe
enzo <enzo1brich@gmail.com>
2025-10-24 23:12:32 -0300

    storing the current path to the returnBuffer

--- src/app/guard/auth/auth-guard.ts
+++ src/app/guard/auth/auth-guard.ts
@@ -1,25 +1,34 @@
 import { inject } from '@angular/core';
-import { CanActivateChildFn, CanActivateFn, Router } from '@angular/router';
+import { ActivatedRoute, CanActivateChildFn, CanActivateFn, Router } from '@angular/router';
 import { UserService } from '../../service/user/user';
+import { ReturnBuffer } from '../../utils/return-to';
 
 export const authGuard: CanActivateFn = (childRoute, state) => {
   const userService = inject(UserService);
+  const router = inject(Router)
+  const returnBuffer = inject(ReturnBuffer)
   const isLogged: boolean = userService.isLogged()
 
   if (isLogged === true) return true;
   
-  const router = inject(Router)
+  const redirectToBuffer = router.url
+  returnBuffer.setPath(redirectToBuffer)
+
   router.navigate(['/login'])
   return false
 };
 
 export const authGuardChild: CanActivateChildFn = (childRoute, state) => {
   const userService = inject(UserService);
+  const router = inject(Router)
+  const returnBuffer = inject(ReturnBuffer)
   const isLogged: boolean = userService.isLogged()
 
   if (isLogged === true) return true;
   
-  const router = inject(Router)
+  const redirectToBuffer = router.url
+  returnBuffer.setPath(redirectToBuffer)
+
   router.navigate(['/login'])
   return false
 };
