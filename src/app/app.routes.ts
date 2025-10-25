ebf341e94b99af3f950bef181eac6576b6f23e03
enzo <enzo1brich@gmail.com>
2025-10-24 23:13:18 -0300

    static titles + dinamic title w/ titleResolver

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,26 +1,32 @@
 import { Routes } from '@angular/router';
 import { ProfileRoutes } from './page/profile/profile.routes';
+import { titleResolverFactory } from './utils/title-resolver';
 
 export const routes: Routes = [
     {
         path: '',
-        loadComponent: () => import('./page/product/landing/landing').then(m => m.Landing)
+        loadComponent: () => import('./page/product/landing/landing').then(m => m.Landing),
+        title: 'VerdiQ'
     },
     {
         path: 'login',
-        loadComponent: () => import('./page/auth/login/login').then(m => m.LoginPage)
+        loadComponent: () => import('./page/auth/login/login').then(m => m.LoginPage),
+        title: 'Login'
     },
     {
         path: 'register',
-        loadComponent: () => import('./page/auth/register/register').then(m => m.RegisterPage)
+        loadComponent: () => import('./page/auth/register/register').then(m => m.RegisterPage),
+        title: 'Registro'
     },
     {
         path: 'product/:productId',
-        loadComponent: () => import('./page/product/product/product').then(m => m.ProductPage)
+        loadComponent: () => import('./page/product/product/product').then(m => m.ProductPage),
+        title: titleResolverFactory('product')
     },
     {
         path: 'store/:storeId',
-        loadComponent: () => import('./page/store/store/store').then(m => m.StorePage)
+        loadComponent: () => import('./page/store/store/store').then(m => m.StorePage),
+        title: titleResolverFactory('store')
     },
     {
         path: 'create-store',
